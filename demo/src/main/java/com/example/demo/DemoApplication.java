add1a69dbed2f242697670b86c7748b6088d3cd4
hyennin <chm050202@gmail.com>
2023-03-09 12:17:28 +0900

    IoC Container, 어노테이션

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = { "hello", "Bean" })
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		// System.out.println(context.getBean("myBean"));
+		String[] beanNames = context.getBeanDefinitionNames();
+		for(String name: beanNames) {
+			if (!(name.startsWith("org.") || name.startsWith("spring.")))
+				System.out.println(name);
+		}
+
+		MyBean bean = (MyBean) context.getBean("myBean");
+		System.out.println(bean);
+//		Person p = (Person) context.getBean(Person.class);
+		Person p = (Person) context.getBean("myPerson");
+		System.out.println(p);
 	}
 
 }
