0418f1d72c2ac071a4535e6c83a8305571d70d47
Hyemin Choi <90095211+hyennin@users.noreply.github.com>
2023-05-18 11:22:09 +0900

    template 엔진 활용

---
+++ thymeleaf/src/main/java/study/thymeleaf/Author.java
@@ -0,0 +1,16 @@
+package study.thymeleaf;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+// Q) Lombok을 활용하여 생성자, 게터, 세터 대체
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author implements Serializable {
+    private String name;
+}
---
+++ thymeleaf/src/main/java/study/thymeleaf/Book.java
@@ -0,0 +1,37 @@
+package study.thymeleaf;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Book implements Serializable {
+    private String name;
+    private Author author;
+    private int price;
+    private boolean sale;
+    private double saleAmount;
+
+    public Book(String authorName, String bookName, int price) {
+        super();
+        this.author = new Author(authorName);
+        this.name = bookName;
+        this.price = price;
+    }
+
+    public Book(String authorName, String bookName, int price, boolean sale, double saleAmount) {
+        super();
+        this.author = new Author(authorName);
+        this.name = bookName;
+        this.price = price;
+        this.sale = sale;
+        this.saleAmount = saleAmount;
+    }
+
+    public boolean isSale() {
+        return sale;
+    }
+}
